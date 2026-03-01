db790b7b3494158cd37780d01c60455b6a4bd1e3
Jake Little <jakelittle135@gmail.com>
2026-02-28 21:18:46 -0800

    Use premade toolchain

--- boot_time.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <chrono>
-
-namespace jsnl {
-
-std::chrono::time_point<std::chrono::system_clock> GetBootTime();
-
-}  // namespace jsnl
--- root.cc
+++ root.cc
@@ -1,7 +1,5 @@
 #include "root.h"
 
-#include <iostream>
-
 #include "pw_async2/basic_dispatcher.h"
 #include "pw_async2/future.h"
 #include "pw_async2/system_time_provider.h"
@@ -9,11 +7,40 @@
 
 namespace jsnl {
 
-int Root() {
-  // Populates static boot_time variable.
-  GetBootTime();
+class SomeTask : public pw::async2::Task {
+ public:
+  SomeTask(pw::chrono::SystemClock::duration duration)
+      : Task(PW_ASYNC_TASK_NAME("SomeTask")), duration_(duration) {}
 
-  std::cerr << "Hello from Root" << std::endl;
+ private:
+  pw::async2::Poll<> DoPend(pw::async2::Context& context) override {
+    // Acquire a future when the task first runs.
+    if (!time_future_.is_pendable()) {
+      time_future_ = pw::async2::GetSystemTimeProvider().WaitFor(duration_);
+    }
+
+    // Wait for the timer to complete.
+    if (time_future_.Pend(context).IsPending()) {
+      PW_LOG_INFO("Waiting…");
+      // Notify the dispatcher that the task is stalled.
+      return pw::async2::Pending();
+    }
+
+    PW_LOG_INFO("Done!");
+    // Notify the dispatcher that the task is complete.
+    return pw::async2::Ready();
+  }
+
+  pw::chrono::SystemClock::duration duration_;
+  pw::async2::TimeFuture<pw::chrono::SystemClock> time_future_;
+};
+
+int Root() {
+  // The cooperative scheduler of pw_async2.
+  pw::async2::BasicDispatcher dispatcher;
+  SomeTask task(std::chrono::seconds(1));
+  dispatcher.Post(task);
+  dispatcher.RunToCompletion();
   return 0;
 }
 
